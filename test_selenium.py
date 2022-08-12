279842427c708817b3fa5b8df9fc7b76ca31f14a
veredaha <97604250+veredaha@users.noreply.github.com>
2022-08-12 11:39:55 +0300

    selenium

--- test_selenium.py
+++ test_selenium.py
@@ -15,7 +15,7 @@ path = 'C:/geckodriver.exe'
 
 
 def open()->None:
-    "finction opens tha clothing store site"
+    "finction opens the clothing store site"
     driver = webdriver.Firefox(executable_path=path) 
     driver.maximize_window()
     driver.get('http://automationpractice.com/index.php?controller=authentication&back=my-account')
@@ -103,6 +103,7 @@ def test_forgat_passwd()->None:
     mylogger.info("test for forget password")
     driver = open()
     driver.find_element(By.LINK_TEXT, "Forgot your password?" ).click()
+    print(driver.title)
     assert driver.title == 'Forgot your password - My Store'
     driver.close()
 
@@ -114,18 +115,24 @@ def test_find_dress()->None:
     driver.find_element(By.NAME, "passwd").send_keys("123456")
     driver.find_element(By.NAME, "SubmitLogin").click()
     assert driver.title == 'My account - My Store'
+    time.sleep(7)
+    account = driver.find_element(By.CLASS_NAME, "account")
+    span = account.find_element(By.TAG_NAME, "span").text
+    assert span == 'Vered Aharonov'
     time.sleep(5)
     prices = []
     driver.find_element(By.NAME, "search_query").send_keys("summer")
     driver.find_element(By.NAME, "submit_search").click()
-    time.sleep(5)
+    time.sleep(7)
     product_list = driver.find_element(By.CLASS_NAME,"product_list")
+    time.sleep(5)
     product_containers = product_list.find_elements(By.CLASS_NAME,"product-container")
     for product_container in product_containers:
         right_block = product_container.find_element(By.CLASS_NAME,"right-block")
         content_price = right_block.find_element(By.CLASS_NAME,"content_price")
         span = content_price.find_element(By.TAG_NAME,"span").text
-        prices.append(span.strip())
+        prices.append(span)
+        time.sleep(4)
     low = min(prices)
     for product_container in product_containers:
         right_block = product_container.find_element(By.CLASS_NAME, "right-block")
@@ -133,22 +140,21 @@ def test_find_dress()->None:
         if low == price:
             right_block.find_element(By.CLASS_NAME, "button-container")
             right_block.find_element(By.TAG_NAME, "a").click()
-            driver.find_element(By.ID,"add_to_cart").click()
-    driver = open()
-    button_container = driver.find_element(By.CLASS_NAME, "button-container")
-    button_container.find_element(By.TAG_NAME,"a").click()
+    time.sleep(3)
     driver = open()
     driver.find_element(By.CSS_SELECTOR,"#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium").click()
     driver.find_element(By.NAME,"processAddress").click()
+    time.sleep(3)
     driver.find_element(By.ID,"uniform-cgv").click()
     time.sleep(5)
-    driver.find_element(By.NAME,"processCarrier").click()
+    driver.find_element(By.NAME,"processCarrier")
     driver.find_element(By.CLASS_NAME,"bankwire").click()
-    right_pass = driver.find_element(By.ID,"cart_navigation")
-    right_pass.find_element(By.TAG_NAME,"button").click()
+    cart = driver.find_element(By.ID,"cart_navigation")
+    cart.find_element(By.TAG_NAME,"button").click()
     time.sleep(5)
     order = driver.find_element(By.CLASS_NAME,"box").text
     assert "Your order on My Store is complete." in order
+
     
    
    
